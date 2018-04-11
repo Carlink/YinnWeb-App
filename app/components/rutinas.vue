a75926cf0a235c2e498a4b5e5f85a7b3ac305480
Carliink <carliianoide@hotmail.com>
2018-04-10 19:18:45 -0500

    Añadido Google Blockly

--- app/components/rutinas.vue
+++ app/components/rutinas.vue
@@ -1,11 +1,28 @@
 <template>
 	<span>
 		Rutinas
+		<div id="blocklyDiv" style="height: 480px; width: 600px;" ref="toolbox">			
+		</div>
 	</span>
 </template>
 
 <script>
-	
+
+	export default{
+		mounted(){
+			var workspacePlayground = Blockly.inject('blocklyDiv',
+      			{toolbox: '<xml id="toolbox" style="display: none">'+
+					  '<block type="controls_if"></block>'+
+					  '<block type="controls_repeat_ext"></block>'+
+					  '<block type="logic_compare"></block>'+
+					  '<block type="math_number"></block>'+
+					  '<block type="math_arithmetic"></block>'+
+					  '<block type="text"></block>'+
+					  '<block type="text_print"></block>'+
+					'</xml>'
+      				});
+		}
+	}
 </script>
 
 <style scoped>
