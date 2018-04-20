e670909730b1cad48f3665c77f3233a0a5331891
Unknown <carliianoide@hotmail.com>
2018-04-20 01:55:36 -0500

    Blockly ya tiene algunos modulos basicos default

--- app/components/rutinas.vue
+++ app/components/rutinas.vue
@@ -5,14 +5,96 @@
 		</div>
 
 		<xml style="display: none;" ref="toolbox">
-			<category name="Control">
+			<!-- <category name="Control"> -->
+				
+				<!-- <block type="controls_repeat_ext"></block> -->
+				<!-- <block type="math_arithmetic"></block> -->
+				
+				
+				
+				
+			<!-- </category> -->
+			<category name="CONTROL">
 				<block type="controls_if"></block>
-				<block type="controls_repeat_ext"></block>
-				<block type="logic_compare"></block>
-				<block type="math_number"></block>
-				<block type="math_arithmetic"></block>
-				<block type="text"></block>
+			</category>
+			<category name="LOGICA">
+				<block type="controls_if"></block>
+			    <block type="logic_compare"></block>
+			    <block type="logic_operation"></block>
+			    <block type="logic_negate"></block>
+			    <block type="logic_boolean"></block>
+			    <block type="logic_null"></block>
+			    <block type="logic_ternary"></block>
+			</category>
+			<category name="TIEMPO">
+				<block type="controls_repeat_ext">
+			        <value name="TIMES">
+			          <shadow type="math_number">
+			            <field name="NUM">10</field>
+			          </shadow>
+			        </value>
+			    </block>
+			    <block type="controls_whileUntil"></block>
+			    <block type="controls_for">
+			        <value name="FROM">
+			          <shadow type="math_number">
+			            <field name="NUM">1</field>
+			          </shadow>
+			        </value>
+			        <value name="TO">
+			          <shadow type="math_number">
+			            <field name="NUM">10</field>
+			          </shadow>
+			        </value>
+			        <value name="BY">
+			          <shadow type="math_number">
+			            <field name="NUM">1</field>
+			          </shadow>
+			        </value>
+			      </block>
+			    <!-- <block type="controls_forEach"></block> -->
+			    <!-- <block type="controls_flow_statements"></block> -->
+			</category>
+			<category name="TEXTO">
+				<block type="text"></block>										
+			</category>
+			<category name="MATEMATICAS">
+				<block type="math_number"></block>	
+				<block type="math_arithmetic">
+			        <value name="A">
+			          <shadow type="math_number">
+			            <field name="NUM">1</field>
+			          </shadow>
+			        </value>
+			        <value name="B">
+			          <shadow type="math_number">
+			            <field name="NUM">1</field>
+			          </shadow>
+			        </value>
+			    </block>
+      			<block type="math_number_property">
+			        <value name="NUMBER_TO_CHECK">
+			          <shadow type="math_number">
+			            <field name="NUM">0</field>
+			          </shadow>
+			        </value>
+			    </block>
+			    <block type="math_round">
+			        <value name="NUM">
+			          <shadow type="math_number">
+			            <field name="NUM">3.1</field>
+			          </shadow>
+			        </value>
+			    </block>  
+			</category>
+			<category name="DEBUG">
 				<block type="text_print"></block>
+			</category>
+			<category name="SWITCHES">
+				
+			</category>
+			<category name="SENSORES">
+				
 			</category>
 		</xml>
 	</span>
@@ -23,6 +105,7 @@
 	export default{
 		mounted(){
 
+			//Aqui creamos el workspace
 			var workspacePlayground = Blockly.inject('blocklyDiv',
       			{
       			toolbox: this.$refs.toolbox,
@@ -35,6 +118,10 @@
 		        trashcan: true,
 		             }				
       			);
+
+			//Aqui mandamos llamar el codigo generado
+			//var code = Blockly.JavaScript.workspaceToCode(workspace);
+
 		}
 	}
 </script>
